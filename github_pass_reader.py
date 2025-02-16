3049f377740db303aab72c633a95f629932e63a1
Mohammad Reza Fekri <92860582+fekri8614@users.noreply.github.com>
2025-02-16 20:51:35 +0330

    Add files via upload

---
+++ github_pass_reader.py
@@ -0,0 +1,32 @@
+import random
+
+def main():
+    file_path = input("file path: ")
+
+    if(len(file_path) > 8):
+        generate_pass(file_path)
+    else:
+        print("Try again.")
+        main()
+
+def generate_pass(file_path):
+    pass_list = []
+    filtered_pass_list = []
+    try:
+        with open(file_path, "r") as files:
+            for file in files:
+                pass_list.append(file)
+
+            for pass_item in pass_list:
+                filtered_pass_list.append(pass_item.replace("\n", ""))
+
+        print("\n" + "-"*4 + " Result " + "-"*4)
+        print("Your Password: " + str(random.choice(filtered_pass_list)))
+        
+    except Exception as e:
+        print("\n" + "-"*10 + " Occured Error " + "-"*10)
+        print(e) 
+
+if __name__ == "__main__":
+    main()
+
